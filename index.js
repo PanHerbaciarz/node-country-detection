0668785081ddd4a482229bf22080b86dead53a90
Dominik Zdziarski <kontakt@herbaciarz.com>
2017-05-07 17:50:55 +0200

    Stable version

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Ok, let's rock!");
+const ncd = require('./node-country-detection');
+const http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write(ncd.getCountry(req) + ' ' + ncd.getCurrency(req));
+    res.end();
+}).listen(3000);
